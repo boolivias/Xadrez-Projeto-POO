388f583e733a5bff1d05491e4ebe41a53b4f230f
boolivias <jean_wylmer@hotmail.com>
2020-09-30 15:06:10 -0300

    Criado e implementado a classe Jogador

---
+++ Jogador.java
@@ -0,0 +1,38 @@
+public class Jogador {
+    private String nome;
+    private char cor;
+
+    /**
+     * A classe peça só será implementada na segunda parte do projeto
+     */
+    // private int[] indexPecas;
+
+    // public int[] pecasAtivas(Peca[] pecas) {
+    /**
+     * percorre a lista de pecas de acordo com os valores em indexPecas chamando o
+     * método estaAtivo da peca, armazenando o index de todas as pecas ativas e
+     * retornando o vetor com o index de todas as pecas ativas
+     */
+    // }
+
+    public Jogador(String nome, char cor) {
+        this.nome = nome;
+        this.cor = cor;
+    }
+
+    public char getCor() {
+        return this.cor;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setCor(char cor) {
+        this.cor = cor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
