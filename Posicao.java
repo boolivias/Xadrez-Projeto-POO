bd8e0c778106339440d81d70b726f65dbc5edc42
boolivias <jean_wylmer@hotmail.com>
2020-09-30 10:18:17 -0300

    criado e implementado a classe posicao

---
+++ Posicao.java
@@ -0,0 +1,37 @@
+public class Posicao {
+    private int indexPecaPresente;
+    private char linha;
+    private int coluna;
+    private char cor;
+
+    public Posicao(char linha, int coluna, char cor) {
+        this.linha = linha;
+        this.coluna = coluna;
+        this.cor = cor;
+        this.indexPecaPresente = -1;
+    }
+
+    public char getLinha() {
+        return this.linha;
+    }
+
+    public int getColuna() {
+        return this.coluna;
+    }
+
+    public char getCor() {
+        return this.cor;
+    }
+
+    public void ocupa(int indexPeca) {
+        this.indexPecaPresente = indexPeca;
+    }
+
+    public void desocupa() {
+        this.indexPecaPresente = -1;
+    }
+
+    public boolean estaOcupada() {
+        return this.indexPecaPresente != -1;
+    }
+}
