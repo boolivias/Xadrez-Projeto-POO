a54cb8529cac1dd1b25a725e4eb7914f70f78a45
boolivias <jean_wylmer@hotmail.com>
2020-11-30 22:29:06 -0300

    Exclusao de metodos nao usados e utilizando a classe Constantes

--- Posicao.java
+++ Posicao.java
@@ -1,37 +1,21 @@
+import Util.Constantes;
+
 public class Posicao {
-    private int indexPecaPresente;
     private char coluna;
     private int linha;
     private char cor;
 
-    public Posicao(int coluna, int linha, char cor) {
+    public Posicao(int linha, int coluna, char cor) {
         this.coluna = (char) coluna;
         this.linha = linha;
         this.cor = cor;
-        this.indexPecaPresente = -1;
     }
 
-    public char getcoluna() {
-        return this.coluna;
-    }
+    public void imprimir() {
+        System.out.print(this.cor == Constantes.COR_PRETO ? Constantes.BG_PRETO : Constantes.BG_BRANCO);
 
-    public int getColuna() {
-        return this.linha;
-    }
+        System.out.print("   ");
 
-    public char getCor() {
-        return this.cor;
-    }
-
-    public void ocupa(int indexPeca) {
-        this.indexPecaPresente = indexPeca;
-    }
-
-    public void desocupa() {
-        this.indexPecaPresente = -1;
-    }
-
-    public boolean estaOcupada() {
-        return this.indexPecaPresente != -1;
+        System.out.print(Constantes.BG_RESETA);
     }
 }
