3da0322f721be1eb3f2c6c2f46d7a39ac2d3f03f
boolivias <jean_wylmer@hotmail.com>
2020-12-01 00:13:34 -0300

    Implementaçao da representação da peca presente na posicao com associaçao

--- Posicao.java
+++ Posicao.java
@@ -1,20 +1,33 @@
+import Pecas.Peca;
 import Util.Constantes;
 
 public class Posicao {
     private char coluna;
     private int linha;
     private char cor;
+    private Peca pecaPresente;
 
     public Posicao(int linha, int coluna, char cor) {
+        this._constructor(linha, coluna, cor, null);
+    }
+
+    public Posicao(int linha, int coluna, char cor, Peca peca) {
+        this._constructor(linha, coluna, cor, peca);
+    }
+
+    private void _constructor(int linha, int coluna, char cor, Peca peca) {
         this.coluna = (char) coluna;
         this.linha = linha;
         this.cor = cor;
+        this.pecaPresente = peca;
     }
 
     public void imprimir() {
         System.out.print(this.cor == Constantes.COR_PRETO ? Constantes.BG_PRETO : Constantes.BG_BRANCO);
 
-        System.out.print("   ");
+        System.out.print(" ");
+        System.out.print(pecaPresente != null ? pecaPresente.desenho() : " ");
+        System.out.print(" ");
 
         System.out.print(Constantes.BG_RESETA);
     }
