ea0304b05e0eb54b0950b0740a7f3d76f2d05b1a
boolivias <jean_wylmer@hotmail.com>
2020-12-06 21:50:30 -0300

    Correçao no instaciamento das peças e dos jogadores, criado metodo de posicionar as peças inicialmente

--- Jogo.java
+++ Jogo.java
@@ -13,12 +13,14 @@ public class Jogo {
         this.pecas = criaPecas();
         Peca[] pBrancas = Arrays.copyOfRange(this.pecas, 0, 15);
         Peca[] pPretas = Arrays.copyOfRange(this.pecas, 16, 32);
+        this.jogadores = new Jogador[2];
         jogadores[0] = new Jogador(nomeJogador1, corJogador1,
                 corJogador1 == Constantes.COR_BRANCO ? pBrancas : pPretas);
         jogadores[1] = new Jogador(nomeJogador2, corJogador2,
                 corJogador2 == Constantes.COR_BRANCO ? pBrancas : pPretas);
         this.vezJogador = 0;
         this.tabuleiro = new Tabuleiro();
+        posicionaPecas();
         this.status = 0;
     }
 
@@ -39,17 +41,16 @@ public class Jogo {
             // Pulo entre as pecas pretas e brancas
             i *= 16;
 
-            for (int j = 0; j < 2; j++) {
-                // Pulo entre as peças que se repetem
-                j *= 5;
-
-                peca[(0 + j) + i] = new Torre(cor);
-                peca[(1 + j) + i] = new Cavalo(cor);
-                peca[(2 + j) + i] = new Bispo(cor);
-            }
+            peca[0 + i] = new Torre(cor);
+            peca[1 + i] = new Cavalo(cor);
+            peca[2 + i] = new Bispo(cor);
 
             peca[3 + i] = new Rei(cor);
             peca[4 + i] = new Dama(cor);
+
+            peca[5 + i] = new Bispo(cor);
+            peca[6 + i] = new Cavalo(cor);
+            peca[7 + i] = new Torre(cor);
 
             for (int j = 8; j < 16; j++) {
                 peca[j + i] = new Peao(cor);
@@ -58,5 +59,24 @@ public class Jogo {
             cor = Constantes.COR_PRETO;
         }
         return peca;
+    }
+
+    private void posicionaPecas() {
+        int lin = 0;
+        int indexPecas = 0;
+        while (true) {
+            for (int col = 0; col < 8; col++) {
+                this.tabuleiro.definePecaPosicao(lin, col, this.pecas[indexPecas++]);
+            }
+
+            if (lin == 0)
+                lin++;
+            else if (lin == 1)
+                lin = 7;
+            else if (lin == 7)
+                lin--;
+            else
+                return;
+        }
     }
 }
